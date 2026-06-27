9ed669b47ff94822e5e0630beafc4bd58e23382d
Sean McKinney <samckinn@outlook.com>
2026-06-27 14:47:37 -0500

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE='geo-test-v1';
+const CACHE='hunt-v1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(h=>h||fetch(e.request)).catch(()=>caches.match('./index.html')));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(h=>h||fetch(e.request)).catch(()=>caches.match('./index.html')));});
